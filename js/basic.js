552e07d4118dcd34094dfe88f654f6bc3b9cf288
Rushikesh  B Sambare <rushisambare91211@gmail.com>
2024-02-08 15:41:53 +0530

    the even number program save

--- js/basic.js
+++ js/basic.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+// write the program to find even number 
+const prompt = require("prompt-sync")();
+let num=prompt("enter your number");
+if(num%2==0){
+    console.log("the given number is even");
+}
+else{
+    console.log("the given number id odd")
+}
